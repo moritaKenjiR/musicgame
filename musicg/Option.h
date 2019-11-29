c839fa76384f7494578b28a090e93cb0a1fc21a4
moritaKenjiR <dotfuture.bm2dx@gmail.com>
2019-11-29 13:05:34 +0900

    a

--- musicg/Option.h
+++ musicg/Option.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Geometry.h"
 
 enum class LaneType {
 	L,
@@ -13,5 +14,31 @@ enum class noteType
 {
 	Normal,
 	Long,
+	Side_L,
+	Side_R,
 	max,
+};
+
+class Option
+{
+public:
+	~Option();
+	static Option& Instance()
+	{
+		static Option Instance;
+		return Instance;
+	}
+
+	void Update();
+	
+	void SetLaneMovePos(int MovePos);
+	Position2 GetOffset();
+private:
+	Option();
+	Option(const Option&);
+	void operator=(const Option&);
+
+	int _nowLanePos;
+	int _moveLanePos;
+	Position2 _laneOffset;
 };
